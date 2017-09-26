dff1cb97b762badb78566fa3f02b54d27fe00964
Android-xiaole <932637491@qq.com>
2017-09-26 16:05:42 +0800

    在application里面增加了log日志的控制开关

--- public_jar/src/main/java/com/le/base/BaseApplication.java
+++ public_jar/src/main/java/com/le/base/BaseApplication.java
@@ -11,6 +11,8 @@ import android.provider.Settings;
 import android.support.multidex.MultiDex;
 import android.widget.Toast;
 
+import com.blankj.utilcode.BuildConfig;
+import com.blankj.utilcode.util.LogUtils;
 import com.blankj.utilcode.util.Utils;
 import com.le.utils.MLog;
 import com.le.utils.UtilFile;
@@ -48,6 +50,20 @@ public class BaseApplication extends Application{
 //        AutoLayoutConifg.getInstance().useDeviceSize();
 
         Utils.init(this);
+        LogUtils.Config config = LogUtils.getConfig()
+//                .setLogSwitch(BuildConfig.DEBUG)// 设置log总开关，包括输出到控制台和文件，默认开
+//                .setConsoleSwitch(BuildConfig.DEBUG)// 设置是否输出到控制台开关，默认开
+//                .setGlobalTag(null)// 设置log全局标签，默认为空
+//                // 当全局标签不为空时，我们输出的log全部为该tag，
+//                // 为空时，如果传入的tag为空那就显示类名，否则显示tag
+//                .setLogHeadSwitch(true)// 设置log头信息开关，默认为开
+//                .setLog2FileSwitch(false)// 打印log时是否存到文件的开关，默认关
+//                .setDir("")// 当自定义路径为空时，写入应用的/cache/log/目录中
+//                .setBorderSwitch(true)// 输出日志是否带边框开关，默认开
+//                .setConsoleFilter(LogUtils.V)// log的控制台过滤器，和logcat过滤器同理，默认Verbose
+//                .setFileFilter(LogUtils.V)// log文件过滤器，和logcat过滤器同理，默认Verbose
+        ;
+        LogUtils.d(config.toString());
         CrashReport.initCrashReport(getApplicationContext(), "you register id", BaseConfig.DEBUG);
 
         System.gc();
